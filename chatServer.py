4055d1819da120b8dd2cd7cc50acc48507a1bd20
Raul Lucero <raul.lucero27@gmail.com>
2014-06-18 23:10:45 -0500

    [add] Cliente del sistema de mensajeria

--- chatServer.py
+++ chatServer.py
@@ -2,14 +2,26 @@ __author__ = 'raul'
 
 import socket,select
 
+def broadcast_msg(sock, msg):
+    for socket in connectionList:
+        if socket != server and socket != sock:
+            try:
+                socket.send(msg)
+            except:
+                socket.close()
+                connectionList.remove(socket)
+
+
+
 if __name__ == "__main__":
-#Declaramos la lista de conexiones que se conecten al servidor y el buffer de mensajes
+
     connectionList = []
     msgBuffer = 4096
 
-    server = socket.socket((socket.AF_INET, socket.SOCK_STREAM))#Se crea el socket del servidor
-    server.sockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)#ReaseADDR dice que reuse el socket
-    server.bind(("", 6969)) # se conecta al localhost y al puerto 6969
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("0.0.0.0", 6969))
+    server.listen(10)
 
     connectionList.append(server)
     print "Servidor iniciado"
@@ -20,5 +32,20 @@ if __name__ == "__main__":
             if sock == server:
                 client, addr = server.accept()
                 connectionList.append(client)
+                print "Usuario (%s:%s) conectado" %addr
+                broadcast_msg(client,"(%s:%s) entro a la sala \n" %addr)
+
+            else:
+                try:
+                    data = sock.recv(msgBuffer)
+                    if data:
+                        broadcast_msg(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+
+                except:
+                    broadcast_msg(sock, "Client (%s, %s) is offline" % addr)
+                    print "Client (%s, %s) is offline" % addr
+                    sock.close()
+                    connectionList.remove(sock)
+                    continue
 
 server.close()
